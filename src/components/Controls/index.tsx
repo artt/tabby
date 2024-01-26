c1ec407d77829137ef0e9e66ab787046a6a727e1
artt <me@artt.dev>
2024-01-26 14:43:58 +0700

    blur handling

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,11 +1,15 @@
+import React from "react"
 import OpenAI from "openai"
 import { cleanUrl } from '../../utils'
 import { IconButton, Input, InputGroup, InputRightElement, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
+import { useDisclosure } from '@chakra-ui/react'
 
 // import { HamburgerIcon } from "./icons"
 import { GiHamburgerMenu } from "react-icons/gi";
 import { ImMakeGroup } from "react-icons/im";
+import { ImUngroup } from "react-icons/im";
 import { MdOutlineDeleteSweep } from "react-icons/md";
+import { FaSortAmountDownAlt } from "react-icons/fa";
 
 import "./style.scss"
 
@@ -59,26 +63,74 @@ async function group() {
   }
 }
 
+async function ungroup() {
+  const window = await chrome.windows.getCurrent({populate: true})
+  chrome.tabs.ungroup(window.tabs!.map(tab => tab.id!))
+}
+
 // https://github.com/Litee/prevent-duplicate-tabs-chrome-extension/blob/master/src/background.ts
 async function deduplicate() {
   chrome.tabs.query({}, tabs => {
     const alreadyEncounteredTabUrls = new Set()
     tabs.forEach(tab => {
-        if (alreadyEncounteredTabUrls.has(tab.url)) {
-            chrome.tabs.remove(tab.id!);
-        }
-        alreadyEncounteredTabUrls.add(tab.url);
+      if (alreadyEncounteredTabUrls.has(tab.url)) {
+          chrome.tabs.remove(tab.id!);
+      }
+      alreadyEncounteredTabUrls.add(tab.url);
     })
   })
   // TODO: report how many tabs are closed in a toast
   // TODO: needo to check if the tabs really are the same?
 }
 
+function sortTabs(a: chrome.tabs.Tab, b: chrome.tabs.Tab) {
+  if (!a.url) return 1
+  if (!b.url) return -1
+  if (a.url < b.url) return -1
+  if (a.url > b.url) return 1
+  return 0
+}
+
+async function sort() {
+  
+  const window = await chrome.windows.getCurrent({populate: true})
+  const tabGroups = await chrome.tabGroups.query({windowId: window.id})
+
+  // loop over tabGroups and sort by url of each tabgroup
+  for (let i = 0; i < tabGroups.length; i++ ) {
+    console.log(`Sorting tabs for group ${i}`)
+    const tabs = await chrome.tabs.query({groupId: tabGroups[i].id})
+    const firstIndex = tabs[0].index
+    tabs.sort(sortTabs)
+    // move the tabs to the sorted order
+    chrome.tabs.move(tabs.map(tab => tab.id!), {index: firstIndex})
+  }
+
+  // now sort ungrouped tabs
+  const tabs = await chrome.tabs.query({groupId: -1})
+  tabs.sort(sortTabs)
+  chrome.tabs.move(tabs.map(tab => tab.id!), {index: -1})
+
+}
+
 export function Controls({searchString, setSearchString}: {searchString: string, setSearchString: React.Dispatch<React.SetStateAction<string>>}) {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
     setSearchString(event.target.value)
   }
+
+  // on blur, close the menu
+  React.useEffect(() => {
+    function handleBlur(e: FocusEvent) {
+      if (e.target === window) {
+        onClose()
+      }
+    }
+    window.addEventListener("blur", handleBlur)
+    return () => {window.removeEventListener("blur", handleBlur)}
+  }, [onClose])
 
   return(
     <div className="controls-container">
@@ -95,7 +147,7 @@ export function Controls({searchString, setSearchString}: {searchString: string,
           pr='2rem'
         />
         <InputRightElement>
-          <Menu>
+          <Menu isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
             {({ isOpen }) => (
               // https://github.com/chakra-ui/chakra-ui/issues/4186#issuecomment-1762955580
               <>
@@ -109,10 +161,16 @@ export function Controls({searchString, setSearchString}: {searchString: string,
               />
               <MenuList display={isOpen ? "block" : "none"}>
                 <MenuItem icon={<ImMakeGroup />} command='⌘G' onClick={group}>
-                  Group tabs in current window
+                  Group tabs
+                </MenuItem>
+                <MenuItem icon={<ImUngroup />} command='⌘U' onClick={ungroup}>
+                  Ungroup tabs
                 </MenuItem>
                 <MenuItem icon={<MdOutlineDeleteSweep />} command='⌘D' onClick={deduplicate}>
                   Remove duplicate tabs
+                </MenuItem>
+                <MenuItem icon={<FaSortAmountDownAlt />} command='⌘S' onClick={sort}>
+                  Sort tabs by URL
                 </MenuItem>
               </MenuList>
               </>
@@ -123,3 +181,4 @@ export function Controls({searchString, setSearchString}: {searchString: string,
     </div>
   )
 }
+
